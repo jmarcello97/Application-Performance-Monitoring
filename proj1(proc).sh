b0a3aee7855bdfc46984be666357cf911f07dbc7
jmarcello97 <jkm7264@rit.edu>
2019-02-28 15:12:29 -0500

    Update proj1(proc).sh

--- proj1(proc).sh
+++ proj1(proc).sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
-./"$1" "$2" &
+spawn () {
+	./APM1 $1 &
+	#./APM2 $2 &
+	#./APM3 $3 &
+	#./APM4 $4 &
+	#./APM5 $5 &
+	#./APM6 $6 &
+}
 
-pid=$!
+#pid=$!
 
-function getprocstats () {
-	ps -eo pid,ppid,cmd,%mem,%cpu | egrep "$pid" > temp.txt
+getprocstats () {
+	ps -eo pid,ppid,cmd,%mem,%cpu | egrep "$1" > temp.txt
 	lines=$( <temp.txt wc -l )
 	tr -s ' ' ',' <temp.txt > temp2.txt
 	cat temp2.txt > temp.txt
@@ -18,7 +25,7 @@ function getprocstats () {
 		line=$( head -n 1 temp.txt )
 		pid1=$( echo "$line" | cut -d "," -f 1 )
 		ppid=$( echo "$line" | cut -d "," -f 2 )
-		if [ $pid -eq $pid1 ] || [ $pid -eq $ppid ]
+		if [ $1 -eq $pid1 ] || [ $1 -eq $ppid ]
 		then
 			echo "$line" >> output.txt
 		fi
@@ -28,8 +35,28 @@ function getprocstats () {
 		cat temp2.txt > temp.txt
 		rm temp2.txt 
 	done
-	kill $pid 2> /dev/null
+	#kill $1 1> /dev/null
 	rm temp.txt
 }
 
-getprocstats
+get_network_stats () {
+	stat=$(ifstat | grep ens33)
+
+	rx=$(echo $stat | awk '{print $6;}')
+	tx=$(echo $stat | awk '{print $8;}')	
+
+	echo $rx
+	echo $tx
+}
+
+main () {
+	spawn $1
+	get_network_stats	
+	#while 
+	#do
+	#	sleep 5
+		#getprocstats "APM1"
+	#done
+}
+
+main $1
