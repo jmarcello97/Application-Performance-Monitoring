377865145a9d238754d63dcdafc5f774ffa8995d
root <root@localhost.localdomain>
2019-03-05 23:44:16 -0500

    gdanskis stuff (finished i think)

---
+++ taskautoprojv2.sh
@@ -0,0 +1,119 @@
+#!/bin/bash
+
+cleanup () {
+	pkill -9 APM
+
+	rm "pids.txt" 2> /dev/null
+	
+	#alternative cleanup code
+
+	#ps aux | grep APM > cleanuptemp.txt
+	#num_lines=$(wc -l cleanuptemp.txt | awk '{print $1}')
+	#echo $num_lines
+	#while [ $num_lines -ne 0 ]
+	#do
+	#	line=$(head -n 1 cleanuptemp.txt | awk '{print $2}')
+	#	kill $line
+	#	tail -n +2 cleanuptemp.txt > temp2.txt
+	#	cp temp2.txt cleanuptemp.txt
+		
+	# 	num_lines=$(( $num_lines-1 ))
+	#done
+}
+trap cleanup EXIT
+
+spawn () {
+	rm pids.txt 2> /dev/null
+	./APM1 $1 &
+	echo "$!" >> pids.txt
+	./APM2 $1 &
+	echo "$!" >> pids.txt
+	./APM3 $1 &
+	echo "$!" >> pids.txt
+	./APM4 $1 &
+	echo "$!" >> pids.txt
+	./APM5 $1 &
+	echo "$!" >> pids.txt
+	./APM6 $1 &
+	echo "$!" >> pids.txt
+}
+
+#pid=$!
+#parameters = (pid)
+getprocstats () {
+	ps -eo pid,ppid,cmd,%mem,%cpu | egrep "$1" > temp.txt
+	lines=$( <temp.txt wc -l )
+	tr -s ' ' ',' <temp.txt > temp2.txt
+	cat temp2.txt > temp.txt
+	rm temp2.txt
+	cat temp.txt | cut -c 2- > temp2.txt
+	cat temp2.txt > temp.txt
+	rm temp2.txt
+	while [ $lines -ne 0 ]
+	do
+		line=$( head -n 1 temp.txt )
+		pid1=$( echo "$line" | cut -d "," -f 1 )
+		path=$( echo "$line" | cut -d "," -f 3 )
+		len=$( echo ${#path} )
+		(( len-= 2 ))
+		name=${path: -$len}
+		mem=$( echo "$line" | cut -d "," -f 5 )
+		cpu=$( echo "$line" | cut -d "," -f 6 )
+
+		if [ $1 -eq $pid1 ] 
+		then
+			if [ ! -f "$name"_metrics.csv ]
+			then
+				echo "seconds,%CPU,%memory" > "$name"_metrics.csv
+			fi
+
+			echo "$SECONDS,$cpu,$mem" >> "$name"_metrics.csv
+		fi
+
+		(( lines-- ))
+		tail -n "$lines" temp.txt > temp2.txt 
+		cat temp2.txt > temp.txt
+		rm temp2.txt 
+	done
+	rm temp.txt
+}
+
+get_system_stats () {
+	stat=$(ifstat | grep ens33)
+
+	rx=$(echo $stat | awk '{print $6;}')
+	tx=$(echo $stat | awk '{print $8;}')	
+
+	iostat=$(iostat | grep sda)
+	
+	kbs=$(echo $iostat | awk '{print $4;}')
+
+	df=$(df -m | grep /dev/mapper/centos-root)
+	
+	util=$(echo $df | awk '{print $4;}')
+
+	echo "$SECONDS,$rx,$tx,$kbs,$util" >> system_metrics.csv 
+}
+
+main () {
+	spawn $1
+	echo "" > system_metrics.csv
+	all_lines=$( cat "pids.txt" )
+	sleep 5	
+	while [ true ] 
+	do 
+		echo "getting stats"
+		get_system_stats $1
+		echo "system stats gotten"
+		for line1 in $all_lines;
+		do
+			getprocstats $line1
+		done
+		echo "stats gotten"
+		sleep 5
+	
+	done
+}
+
+
+main $1
